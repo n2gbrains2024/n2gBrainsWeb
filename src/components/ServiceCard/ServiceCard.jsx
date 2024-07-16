6c26616958d1bb06a735e2750d3d16862ee91103
hrach347 <hrachpapyan347@gmail.com>
2024-07-16 16:04:11 +0400

    added new colors merged service page

--- src/components/ServiceCard/ServiceCard.jsx
+++ src/components/ServiceCard/ServiceCard.jsx
@@ -1,14 +1,14 @@
-import styles from "./ServiceCard.module.css"
-import orangeStar from "../../assets/orangeStar.svg"
-import gradientStar from "../../assets/gradientStar.svg"
+import styles from "./ServiceCard.module.css";
+import greenStar from "../../assets/greenStar.svg";
+import gradientStar from "../../assets/gradientStar.svg";
 
 function ServiceCard({ text, key }) {
-    return (
-        <div className={styles.card} key={key}>
-            <img src={orangeStar} alt=""/>
-            <img src={gradientStar} alt="" className={styles.gradient}/>
-            <p>{text}</p>
-        </div>
-    )
+  return (
+    <div className={styles.card} key={key}>
+      <img src={greenStar} alt="" />
+      <img src={gradientStar} alt="" className={styles.gradient} />
+      <p>{text}</p>
+    </div>
+  );
 }
-export default ServiceCard
+export default ServiceCard;
--- src/ui/Main/Main.jsx
+++ src/ui/Main/Main.jsx
@@ -1,31 +1,33 @@
-import mainStyles from "./Main.module.css"
-import RunnerText from "../../components/RunnerText/RunnerText.jsx"
-import CourseCard from "../../components/CourseCard/CourseCard.jsx"
-import BlackComponent from "../BlackComponent/BlackComponent.jsx"
-import Services from "../Services/Services.jsx"
-import Contact from "../../ui/Contact/Contact"
-import We from "../We/We.jsx"
-import BigCard from "../../components/BigCard/BigCard.jsx"
-import LandingComponent from "../../components/LandingComponent/LandingComponent.jsx"
-import Students from "../Students/Students.jsx"
-import Feedbacks from "../Feedbacks/Feedbacks.jsx"
+import mainStyles from "./Main.module.css";
+import RunnerText from "../../components/RunnerText/RunnerText.jsx";
+import CourseCard from "../../components/CourseCard/CourseCard.jsx";
+import BlackComponent from "../BlackComponent/BlackComponent.jsx";
+import Services from "../Services/Services.jsx";
+import Contact from "../../ui/Contact/Contact";
+import We from "../We/We.jsx";
+import BigCard from "../../components/BigCard/BigCard.jsx";
+import LandingComponent from "../../components/LandingComponent/LandingComponent.jsx";
+import Students from "../Students/Students.jsx";
+import Feedbacks from "../Feedbacks/Feedbacks.jsx";
+import Footer from "../Footer/Footer.jsx";
 
 function Main() {
-    return (
-        <div className={mainStyles.main}>
-            <LandingComponent />
-            <BigCard />
-            <RunnerText />
-            <CourseCard />
-            <BlackComponent />
-            <Services />
-            <Contact />
-            <Students/>
-            <Feedbacks/>
-            <We />
-            <div className={mainStyles.footerLine}>helllooo</div>
-        </div>
-    )
+  return (
+    <div className={mainStyles.main}>
+      <LandingComponent />
+      <BigCard />
+      <RunnerText />
+      <CourseCard />
+      <BlackComponent />
+      <Services />
+      <Contact />
+      <Students />
+      <Feedbacks />
+      <We />
+      <div className={mainStyles.footerLine}></div>
+      <Footer />
+    </div>
+  );
 }
 
-export default Main
+export default Main;
