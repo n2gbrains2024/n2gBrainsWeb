4d9dbe3a899c02d55ea62da14c9cd4241378905f
hrach347 <hrachpapyan347@gmail.com>
2024-07-29 01:13:30 +0400

    finished media of We page, and fixed some bugs in feedbacks

--- src/ui/We/We.jsx
+++ src/ui/We/We.jsx
@@ -5,16 +5,15 @@ import "animate.css/animate.compat.css";
 function We(params) {
   return (
     <section className={styles.container}>
-      <div className={styles.text}>
+      <div className={styles.texts}>
         <ScrollAnimation animateIn="fadeInRight" animateOut="fadeOut">
           <h2>ԱՀԱ ԵՎ ՄԵՆՔ</h2>
         </ScrollAnimation>
         <p>
           Հրաշալի մասնագետների ֆորմուլա մեկ վայրում, կրեատիվ և ստեղծարար թիմ։
-          Մեր նպատակն է Մեր նպատակն է Մեր նպատակն է Մեր նպատակն է
         </p>
       </div>
-      <div className={styles.imgContainer}></div>
+      <img className={styles.image} />
     </section>
   );
 }
