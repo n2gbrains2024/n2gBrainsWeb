ae2ad8424bce433860316ff31e04c82fc9fe3b3f
hrach347 <hrachpapyan347@gmail.com>
2024-08-12 13:27:01 +0400

    updated and finished contact ui part,fixed media and input floating label

--- src/ui/Contact/Contact.jsx
+++ src/ui/Contact/Contact.jsx
@@ -1,25 +1,24 @@
-import contactStyles from './Contact.module.css' 
-import bigStar from "../../assets/bigStar.svg" 
-import InputComponent from '../../components/InputComponent/InputComponent'
- 
-function Contact(){ 
-  return ( 
-    <section className={contactStyles.container}> 
-      <div className={contactStyles.data}> 
-        <h2>Ստացիր <span>անվճար</span> խորհրդատվություն</h2> 
-        <form className={contactStyles.form}> 
-          <InputComponent placeholder='Անուն Ազգանուն'/>
-          <InputComponent placeholder='Հեռախոսահամար'/>
-          <InputComponent placeholder='Էլ․ հասցե'/>
-          <button>Ուղարկել</button> 
-        </form> 
- 
-      </div> 
-      <div className={contactStyles['img-container']}> 
-        <img src={bigStar} /> 
-      </div>    
-    </section> 
-  ) 
-} 
- 
-export default Contact
+import styles from "./Contact.module.css";
+import bigStar from "../../assets/bigStar.svg";
+import InputComponent from "../../components/InputComponent/InputComponent";
+
+function Contact() {
+  return (
+    <section className={styles.container}>
+      <div className={styles.data}>
+        <h2>
+          Ստացիր <span>անվճար</span> խորհրդատվություն
+        </h2>
+        <form className={styles.form}>
+          <InputComponent placeholder="Անուն Ազգանուն" />
+          <InputComponent placeholder="Հեռախոսահամար" />
+          <InputComponent placeholder="Էլ․ հասցե" />
+          <button>Ուղարկել</button>
+        </form>
+      </div>
+      <img src={bigStar} alt="" className={styles.img} />
+    </section>
+  );
+}
+
+export default Contact;
