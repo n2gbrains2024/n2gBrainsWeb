795cb91f6675fec6bf6bc6c1e6bb25410ade01ef
hrach347 <hrachpapyan347@gmail.com>
2024-08-30 17:23:09 +0400

    updated header menu

--- src/ui/Menu/Menu.jsx
+++ src/ui/Menu/Menu.jsx
@@ -1,9 +1,10 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 import styles from "./Menu.module.css";
 import n2gLogo from "../../assets/n2gWhiteLittleIcon.svg";
 import menuIcon from "../../assets/menuIcon.svg";
-import { useState } from "react";
+import userFace from "../../assets/userFace.svg";
 
 function Menu() {
   const [openMenu, setOpenMenu] = useState(false);
@@ -13,12 +14,17 @@ function Menu() {
   return (
     <div className={styles.menu}>
       <div className={styles.menuItems}>
-        <Link to="/">Գլխավոր</Link>
-        <Link to="/services">Ծառայություններ</Link>
-        <Link to="/portfolio">Պորտֆոլիո</Link>
-        <Link to="/team">Թիմ</Link>
-        <Link to="/about">Մեր մասին</Link>
-        <Link to="/contact">Կապ մեզ հետ</Link>
+        <img src={n2gLogo} alt="" />
+        <div className={styles.links}>
+          <Link to="/">Գլխավոր</Link>
+          <Link to="/services">Ծառայություններ</Link>
+          <Link to="/portfolio">Պորտֆոլիո</Link>
+          <Link to="/team">Թիմ</Link>
+          <Link to="/about">Մեր մասին</Link>
+        </div>
+        <div className={styles.user}>
+          <img src={userFace} alt="" />
+        </div>
       </div>
       <div className={styles.mobileMenu}>
         <img src={n2gLogo} alt="" />
