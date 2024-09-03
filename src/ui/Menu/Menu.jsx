7549388ad2e22328679fe2514c33ea50172781eb
hrach347 <hrachpapyan347@gmail.com>
2024-09-03 14:19:14 +0400

    added mobile ui version for header menu

--- src/ui/Menu/Menu.jsx
+++ src/ui/Menu/Menu.jsx
@@ -6,6 +6,7 @@ import styles from "./Menu.module.css";
 import n2gLogo from "../../assets/n2gWhiteLittleIcon.svg";
 import menuIcon from "../../assets/menuIcon.svg";
 import userFace from "../../assets/userFace.svg";
+import closeIcon from "../../assets/close.svg";
 
 function Menu() {
   const [openMenu, setOpenMenu] = useState(false);
@@ -48,12 +49,43 @@ function Menu() {
       <div className={styles.mobileMenu}>
         <img src={n2gLogo} alt="" />
         <img
-          src={menuIcon}
+          src={openMenu ? closeIcon : menuIcon}
           alt=""
           className={styles.menuIcon}
           onClick={toggleMenu}
         />
-        {openMenu ? <div className={styles.openedMenu}></div> : ""}
+        {openMenu ? (
+          <div className={styles.openedMenu}>
+            <div className={styles.mobileMenuLinks}>
+              <Link to="/" onClick={toggleMenu}>
+                Գլխավոր
+              </Link>
+              <div>
+                <Link to="/services" onClick={toggleServiceDropdown}>
+                  Ծառայություններ
+                </Link>
+                {openServiceDropdown ? <ServiceDropdownCard /> : ""}
+              </div>
+              <Link to="/portfolio" onClick={toggleMenu}>
+                Պորտֆոլիո
+              </Link>
+              <Link to="/team" onClick={toggleMenu}>
+                Թիմ
+              </Link>
+              <Link to="/about" onClick={toggleMenu}>
+                Մեր մասին
+              </Link>
+            </div>
+            <div>
+              <a href="tel:+37496691949" className={styles.phoneButton}>
+                +374 96691949
+              </a>
+              <h1>Մուտք գործել</h1>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
