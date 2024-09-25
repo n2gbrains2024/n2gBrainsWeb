a6fed138d25ff3066b1a97df5843ec9a6290c1c9
hrach347 <hrachpapyan347@gmail.com>
2024-09-25 17:09:38 +0400

    batman

--- src/ui/CourseRegistration/Confirmed.jsx
+++ src/ui/CourseRegistration/Confirmed.jsx
@@ -9,7 +9,6 @@ function Confirmed() {
         <img className={styles.checkBox} src={checkBox} alt="" />
         <div className={styles.checkBoxTextContainer}>
           <p className={styles.checkBoxTitle}>
-            {" "}
             Բաց դասը տեղի կունենա դեկտեմբերի 3-ին (19:30)
           </p>
           <p className={styles.checkBoxTitle2}>Հասցե՝ Կոմիտաս Վ․Վաղարշյան 21</p>
