d83a07a373ff02c25b0942fa8032ae6f17b50bf1
SaqoAxayan <saqoaxayan31@gmail.com>
2024-09-25 16:47:18 +0400

    levl up curseRegistration page

---
+++ src/ui/CourseRegistration/Confirmed.jsx
@@ -0,0 +1,22 @@
+import styles from "./styles.module.css";
+import checkBox from "../../assets/checkBox.svg";
+
+function Confirmed() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.checkBoxContainer}>
+        <h2>Ձեր գրանցումը հաստատվեց</h2>
+        <img className={styles.checkBox} src={checkBox} alt="" />
+        <div className={styles.checkBoxTextContainer}>
+          <p className={styles.checkBoxTitle}>
+            {" "}
+            Բաց դասը տեղի կունենա դեկտեմբերի 3-ին (19:30)
+          </p>
+          <p className={styles.checkBoxTitle2}>Հասցե՝ Կոմիտաս Վ․Վաղարշյան 21</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Confirmed;
--- src/ui/CourseRegistration/CourseRegistration.jsx
+++ src/ui/CourseRegistration/CourseRegistration.jsx
@@ -1,4 +1,34 @@
+import Confirmed from "./Confirmed";
+import styles from "./styles.module.css";
+import React, { useState } from "react";
+
 function CourseRegistration() {
-  return <div>aaaaaaaaaaaaaaaa</div>;
+  const [valid, setValid] = useState(true);
+
+  return (
+    <div className={styles.container}>
+      <h2>Գրանցում անվճար բաց դասին</h2>
+      <p>լրացրեք դաշտերը</p>
+      <form className={styles.register}>
+        <input
+          placeholder="Անուն, Ազգանուն"
+          type="text"
+          className={!valid ? styles.invalid : ""}
+        />
+        <input
+          placeholder="Էլ․ Հասցե"
+          type="email"
+          className={!valid ? styles.invalid : ""}
+        />
+        <input
+          placeholder="Հեռախոսահամար"
+          type="number"
+          className={!valid ? styles.invalid : ""}
+        />
+        <button>Գրանցվել անվճար դասին</button>
+      </form>
+    </div>
+    // <Confirmed></Confirmed>
+  );
 }
 export default CourseRegistration;
